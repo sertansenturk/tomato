88f3bda8f76b2370e37e8a4791998e166c9e891c
Sertan Senturk <contact@sertansenturk.com>
2016-03-16 18:58:44 +0100

    Added pitch distribution parameter setter and pitch class computer

--- tomato/audio/AudioAnalyzer.py
+++ tomato/audio/AudioAnalyzer.py
@@ -6,29 +6,18 @@ from notemodel.NoteModel import NoteModel
 from modetonicestimation.PitchDistribution import PitchDistribution
 import numpy as np
 
+
 class AudioAnalyzer(object):
-    '''
-
-    '''
     def __init__(self, verbose=False):
-        '''
-
-        :return:
-        '''
         self.verbose = verbose
 
         # extractors
         self._pitchExtractor = PredominantMelodyMakam()
         self._tonicIdentifier = TonicLastNote()
         self._noteModeler = NoteModel()
-        self._pd_smooth_factor = 7.5
-        self._pd_step_size = 7.5
+        self._pd_params = {'smooth_factor': 7.5, 'step_size': 7.5}
 
     def extract_pitch(self, filename):
-        '''
-
-        :return:
-        '''
         if self.verbose:
             print("Extracting predominant melody of ", filename)
 
@@ -39,11 +28,6 @@ class AudioAnalyzer(object):
         return pitch
 
     def identify_tonic(self, pitch):
-        '''
-
-        :param pitch:
-        :return:
-        '''
         if self.verbose:
             print("Identifying tonic from the predominant melody of ",
                   pitch['source'])
@@ -56,45 +40,48 @@ class AudioAnalyzer(object):
         return tonic
 
     def compute_pitch_distribution(self, pitch, tonic):
-        '''
-        
-        :param pitch:
-        :param tonic:
-        :return:
-        '''
         if self.verbose:
             print("Computing pitch distribution of ", pitch['source'])
 
         return PitchDistribution.from_hz_pitch(
             np.array(pitch['pitch'])[:, 1], ref_freq=tonic['value'],
-            smooth_factor=self._pd_smooth_factor, step_size=self._pd_step_size)
+            smooth_factor=self._pd_params['smooth_factor'],
+            step_size=self._pd_params['step_size'])
+
+    def compute_class_pitch_distribution(self, pitch, tonic):
+        if self.verbose:
+            print("Computing pitch class distribution of ", pitch['source'])
+
+        return self.compute_pitch_distribution(pitch, tonic).to_pcd()
 
     def identify_ahenk(self, tonic, makamstr):
-        '''
-
-        :param tonic:
-        :param makam:
-        :return:
-        '''
         ahenk = AhenkIdentifier.identify(tonic['value'], makamstr)
         ahenk['source'] = tonic['source']
 
         return ahenk
 
     def set_pitch_extractor_params(self, **kwargs):
-        '''
+        if any(key not in self._pitchExtractor.__dict__.keys()
+               for key in kwargs.keys()):
+            raise KeyError("Possible parameters are: " + ', '.join(
+                self._pitchExtractor.__dict__.keys()) + ".")
 
-        :param kwargs:
-        :return:
-        '''
         for key, value in kwargs.items():
             setattr(self._pitchExtractor, key, value)
 
     def set_tonic_identifier_params(self, **kwargs):
-        '''
+        if any(key not in self._tonicIdentifier.__dict__.keys()
+               for key in kwargs.keys()):
+            raise KeyError("Possible parameters are: " + ', '.join(
+                self._tonicIdentifier.__dict__.keys()) + ".")
 
-        :param kwargs:
-        :return:
-        '''
         for key, value in kwargs.items():
             setattr(self._tonicIdentifier, key, value)
+
+    def set_pitch_distibution_params(self, **kwargs):
+        if any(key not in self._pd_params.keys() for key in kwargs.keys()):
+            raise KeyError("Possible parameters are: " + ', '.join(
+                self._pd_params.keys()) + ".")
+
+        for key, value in kwargs.items():
+            self._pd_params[key] = value
