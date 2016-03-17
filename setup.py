607d35ddaa5832656a52a83ef71fdb8df8c0b051
Sertan Senturk <contact@sertansenturk.com>
2016-03-17 23:56:30 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='tomato',
     description='Turkish-Ottoman Makam Music Analysis Toolbox',
     url='http://sertansenturk.com',
     packages=['tomato'],
+    include_package_data=True,
     install_requires=[
         "numpy",
         "matplotlib"
