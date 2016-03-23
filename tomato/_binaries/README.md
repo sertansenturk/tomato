d0ba3678e02c3b4e6532754b932c7df466e71bca
Sertan Senturk <contact@sertansenturk.com>
2016-03-23 14:21:00 +0100

    Closes #10

--- tomato/_binaries/README.md
+++ tomato/_binaries/README.md
@@ -1,3 +1,3 @@
-The binaries are not stored in **tomato**, because they relatively big and it would take too much space to store them in the repository including the versions introduced in each modification. Instead the binaries are hosted in [tomato_binaries](https://github.com/sertansenturk/tomato_binaries). 
+The binaries are not stored in **tomato**, because they relatively big and it would take too much space to store them in the repository including the versions introduced in each modification. Instead the binaries are provided within the releases of the relevant packages. The binaries are downloaded to this folder during the installation process of tomato.
 
-The binaries are downloaded to here from this repository during the installation process of tomato.
+Please refer to [config/binaries.cfg](https://github.com/sertansenturk/tomato/blob/master/config/binaries.cfg) for these releases.
--- tomato/symbolic/SymbTrAnalyzer.py
+++ tomato/symbolic/SymbTrAnalyzer.py
@@ -3,7 +3,8 @@ from symbtrdataextractor.SymbTrDataExtractor import SymbTrReader
 from .. _binaries.MCRCaller import MCRCaller
 import os
 import subprocess
-# import tempfile
+import tempfile
+import json
 
 # instantiate a mcr_caller
 _mcr_caller = MCRCaller()
@@ -16,13 +17,10 @@ class SymbTrAnalyzer(object):
         # extractors
         self._dataExtractor = SymbTrDataExtractor(print_warnings=verbose)
         self._symbTrReader = SymbTrReader()
-        self._phraseSegmentor = self._get_phrase_segmentor_path()
-
-    def get_mbid_from_name(self):
-        pass
+        self._phraseSegmenter = self._get_phrase_segmenter_path()
 
     @staticmethod
-    def _get_phrase_segmentor_path():
+    def _get_phrase_segmenter_path():
         if _mcr_caller.sys_os == 'linux':
             phrase_seg_path = os.path.join(
                 os.path.dirname(os.path.abspath(__file__)), '..',
@@ -35,6 +33,67 @@ class SymbTrAnalyzer(object):
             raise ValueError('Unsupported OS.')
 
         return phrase_seg_path
+
+    def segment_phrase(self, txt_filename, symbtr_name=None):
+        if self.verbose:
+            print("- Automatic phrase segmentation on the SymbTr-txt file: " +
+                  txt_filename)
+
+        # attempt to get the symbtrname from the filename, if it is not given
+        if symbtr_name is None:
+            symbtr_name = os.path.basename(txt_filename)
+
+        # create the temporary input and output files wanted by the binary
+        temp_in_file = self._create_temp_file(
+            '.json', json.dumps([{'path': txt_filename, 'name': symbtr_name}]))
+        temp_out_file = self._create_temp_file('.json', '')
+
+        # get the pretrained model
+        bound_stat_file, fld_model_file = self._get_phrase_seg_training()
+
+        # call the binary
+        proc = subprocess.Popen(
+            ["%s segmentWrapper %s %s %s %s" %
+             (self._phraseSegmenter, bound_stat_file, fld_model_file,
+              temp_in_file, temp_out_file)],
+            stdout=subprocess.PIPE, shell=True, env=_mcr_caller.env)
+
+        (out, err) = proc.communicate()
+
+        # load the results from the temporary file
+        phrase_boundaries = json.load(open(temp_out_file))
+
+        # unlink the temporary files
+        os.unlink(temp_in_file)
+        os.unlink(temp_out_file)
+
+        # check the MATLAB output,
+        # The prints are in segmentWrapper function in the MATLAB code
+        if "segmentation complete!" not in out:
+            raise IOError("The phrase segmentation is unsuccessful. Please "
+                          "check/report the error in the terminal.")
+
+        return phrase_boundaries
+
+    @staticmethod
+    def _create_temp_file(extension, contentstr):
+        fd, temp_path = tempfile.mkstemp(extension)
+        with open(temp_path, 'w') as f:
+            f.write(contentstr)
+        os.close(fd)
+
+        return temp_path
+
+    @staticmethod
+    def _get_phrase_seg_training():
+        bound_stat_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '..', '_models', 'phrase_segmentation', 'boundStat.mat')
+        fld_model_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '..', '_models', 'phrase_segmentation', 'FLDModel.mat')
+
+        return bound_stat_file, fld_model_file
 
     def extract_data(self, txt_filename, mu2_filename, symbtr_name=None,
                      mbid=None, segment_note_bound_idx=None):
@@ -60,35 +119,6 @@ class SymbTrAnalyzer(object):
                          'is_mu2_header_valid': is_mu2_header_valid}
 
         return data, is_data_valid
-
-    def segment_phrase(self, txt_filename):
-        if self.verbose:
-            print("- Automatic phrase segmentation on the SymbTr-txt file: " +
-                  txt_filename)
-
-        bound_stat_file, fld_model_file = self._get_phrase_seg_training()
-
-        # proc = subprocess.Popen(["/srv/dunya/phraseSeg segmentWrapper %s
-        # %s %s %s" % (boundstat, fldmodel, files_json, out_json)],
-        # stdout=subprocess.PIPE, shell=True, env=subprocess_env)
-        proc = subprocess.Popen(["%s unitTest" % (self._phraseSegmentor)],
-                                stdout=subprocess.PIPE, shell=True,
-                                env=_mcr_caller.env)
-
-        (out, err) = proc.communicate()
-
-        return None
-
-    @staticmethod
-    def _get_phrase_seg_training():
-        bound_stat_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            '..', '_models', 'phrase_segmentation', 'boundStat.mat')
-        fld_model_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            '..', '_models', 'phrase_segmentation', 'FLDModel.mat')
-
-        return bound_stat_file, fld_model_file
 
     def set_data_extractor_params(self, **kwargs):
         if any(key not in self._dataExtractor.__dict__.keys()
