e6949124d518a95ce5783af414cbe2da926e7522
Sertan Senturk <contact@sertansenturk.com>
2016-04-06 01:58:39 +0200

    Simplified _check_mcr_config

--- tomato/MCR.py
+++ tomato/MCR.py
@@ -20,14 +20,14 @@ class MCRCaller(object):
         config.read(self.filepath)
         try:
             op_sys, env_var, mcr_path, set_paths = \
-                self._get_mcr_config(config, 'custom')
+                MCRCaller._get_mcr_config(config, 'custom')
         except (IOError, ValueError):
             try:
                 op_sys, env_var, mcr_path, set_paths = \
-                    self._get_mcr_config(config, 'linux_default')
+                    MCRCaller._get_mcr_config(config, 'linux_default')
             except (IOError, ValueError):
                 op_sys, env_var, mcr_path, set_paths = \
-                    self._get_mcr_config(config, 'macosx_default')
+                    MCRCaller._get_mcr_config(config, 'macosx_default')
 
         subprocess_env = os.environ.copy()
         subprocess_env["MCR_CACHE_ROOT"] = "/tmp/emptydir"
@@ -40,30 +40,29 @@ class MCRCaller(object):
                                 env=self.env)
         return proc.communicate()
 
-    @classmethod
-    def _get_mcr_config(cls, config, section_str):
+    @staticmethod
+    def _get_mcr_config(config, section_str):
         op_sys = config.get(section_str, 'sys_os')
         env_var = config.get(section_str, 'env_var')
         mcr_path = config.get(section_str, 'mcr_path')
         set_paths = config.get(section_str, 'set_paths')
-        cls._check_mcr_config(env_var, mcr_path)
+
+        MCRCaller._check_mcr_config(env_var, mcr_path)
 
         return op_sys, env_var, mcr_path, set_paths
 
-    @classmethod
-    def _check_mcr_config(cls, env_var, mcr_path):
-        if env_var and mcr_path:
-            # MCR installation path is not found
-            if not os.path.exists(mcr_path):
-                raise IOError('The mcr path is not found. Please '
-                              'fill the custom section in '
-                              '"tomato/config/mcr_path.cfg" manually.')
-        elif env_var or mcr_path:  # The configuration is wrong
-            raise ValueError('One of the custom fields for the MCR '
+    @staticmethod
+    def _check_mcr_config(env_var, mcr_path):
+        # MCR installation path is not found
+        if not os.path.exists(mcr_path):
+            raise IOError('The mcr path is not found. Please '
+                          'fill the custom section in '
+                          '"tomato/config/mcr_path.cfg" manually.')
+
+        if not bool(env_var):  # The configuration is wrong
+            raise ValueError('One of the fields for the MCR '
                              'path is empty in "tomato/config/mcr_path.cfg". '
                              'Please reconfigure manually.')
-        else:
-            raise ValueError('Empty fields in the section')
 
     def get_binary_path(self, bin_name):
         if self.sys_os == 'linux':
