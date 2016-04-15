e08865f237ec85628ac136e495503d65a0343eb2
Sertan Senturk <contact@sertansenturk.com>
2016-04-15 08:03:43 +0200

    Added musicxml2lilypond conversion

--- tomato/symbolic/ScoreConverter.py
+++ tomato/symbolic/ScoreConverter.py
@@ -1,12 +1,16 @@
-from musicxmlconverter.symbtr2musicxml import SymbTrScore
+from musicxmlconverter import symbtr2musicxml
+from musicxml2lilypond import musicxml2lilypond
 from symbtrdataextractor.reader.SymbTrReader import SymbTrReader
 from symbtrextras.ScoreExtras import ScoreExtras
 from symbtrdataextractor.metadata.MBMetadata import MBMetadata
+
 import musicbrainzngs
 
 
 class ScoreConverter(object):
-    mb_meta_getter = MBMetadata()
+    _mb_meta_getter = MBMetadata()
+    _xml2ly_converter = musicxml2lilypond.ScoreConverter()
+
     @classmethod
     def txt_mu2_to_musicxml(cls, symtr_txt_filename, symbtr_mu2_filename,
                             out=None, symbtr_name=None, mbid=None):
@@ -18,16 +22,29 @@ class ScoreConverter(object):
             mbid_url = ScoreExtras.get_mbids(symbtr_name)[0]
         else:
             try:
-                meta = cls.mb_meta_getter.crawl_musicbrainz(mbid)
+                meta = cls._mb_meta_getter.crawl_musicbrainz(mbid)
                 mbid_url = meta['url']
             except (musicbrainzngs.NetworkError, musicbrainzngs.ResponseError):
                 mbid_url = mbid
 
-        piece = SymbTrScore(symtr_txt_filename, symbtr_mu2_filename,
-                            symbtrname=symbtr_name, mbid_url=mbid_url)
+        piece = symbtr2musicxml.SymbTrScore(
+            symtr_txt_filename, symbtr_mu2_filename, symbtrname=symbtr_name,
+            mbid_url=mbid_url)
 
         xmlstr = piece.convertsymbtr2xml()  # outputs the xml score as string
         if out is None:
             return xmlstr  # return string
         else:
             piece.writexml(out)  # save to filename
+
+    @classmethod
+    def musicxml_to_lilypond(cls, symtr_xml_in, lilypond_out=None,
+                             render_metadata=False):
+        ly_stream, mapping = cls._xml2ly_converter.run(
+            symtr_xml_in, ly_out=lilypond_out, map_out=None,
+            render_metadata=render_metadata)
+
+        if lilypond_out is None:
+            return mapping, ly_stream
+        else:  # ly_stream is already saved to the user-specified file
+            return mapping
