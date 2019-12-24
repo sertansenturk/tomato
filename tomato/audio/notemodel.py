191ffdca3f4bd8046979d7cc874d92c6ae395fd1
Sertan Senturk <contact@sertansenturk.com>
2019-12-24 07:01:21 +0000

    Removed unnecessary utf-8 declarations

--- tomato/audio/notemodel.py
+++ tomato/audio/notemodel.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 
 # Copyright 2016 - 2018 Miraç Atıcı & Sertan Şentürk
 #
--- tomato/plotter.py
+++ tomato/plotter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 
 # Copyright 2016 - 2018 Sertan Şentürk
 #
